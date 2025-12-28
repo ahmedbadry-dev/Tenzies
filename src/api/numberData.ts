50a2b610f02b18f6ee3ddb305773981a82b84bec
ahmedbadry-dev <ahmedbadry.dev@gmail.com>
2025-12-29 00:48:42 +0200

    feat(implement):  button functionality and state management; add number data source

---
+++ src/api/numberData.ts
@@ -0,0 +1,59 @@
+export default [
+    {
+        id: 1,
+        isClicked: false,
+        number: 2
+
+    },
+    {
+        id: 2,
+        isClicked: false,
+        number: 5
+
+    },
+    {
+        id: 3,
+        isClicked: false,
+        number: 1
+
+    },
+    {
+        id: 4,
+        isClicked: false,
+        number: 3
+
+    },
+    {
+        id: 5,
+        isClicked: false,
+        number: 9
+
+    },
+    {
+        id: 6,
+        isClicked: false,
+        number: 7
+
+    },
+    {
+        id: 7,
+        isClicked: false,
+        number: 4
+
+    },
+    {
+        id: 8,
+        isClicked: false,
+        number: 6
+
+    }, {
+        id: 9,
+        isClicked: false,
+        number: 0
+
+    }, {
+        id: 10,
+        isClicked: false,
+        number: 8
+    },
+]
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,22 @@
 
 type ButtonProps = {
-    num: number
+    num: {
+        id: number,
+        isClicked: boolean,
+        number: number
+    },
+    handleButtonClicked(id: number): void
 }
 
-const Button = ({ num }: ButtonProps) => {
+const Button = ({ num, handleButtonClicked }: ButtonProps) => {
+
     return (
         <button
-            className="block bg-zinc-100 shadow cursor-pointer text-3xl p-4 px-5 rounded-xl font-medium "
-        >{num}</button>
+            onClick={() => handleButtonClicked(num.id)}
+            className={`
+                ${num.isClicked ? "bg-green-400" : 'bg-zinc-100'}
+                block  shadow cursor-pointer text-3xl p-4 px-5 rounded-xl font-medium`}
+        >{num.number}</button>
     )
 }
 
