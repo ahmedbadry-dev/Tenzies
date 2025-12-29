fd1444e248fa5edbc1cd3783dc9812aed225c908
ahmedbadry-dev <ahmedbadry.dev@gmail.com>
2025-12-29 04:36:11 +0200

    refactor: simplify state management and improve button functionality in Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,26 +2,31 @@
 import Message from './Message'
 import Button from './Button'
 import numberData from '../api/numberData'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+
+const generateRandomNumber = (): number => {
+  console.log('random called');
+  return Math.floor(Math.random() * 10)
+}
+
 const Main = () => {
 
-  const [numbers, setNumber] = useState(numberData)
-  const [UserIsWin, setUserIsWin] = useState(false)
-
-  const generateRandomNumber = (): number => {
-    return Math.floor(Math.random() * 10)
-  }
+  const [numbers, setNumbers] = useState(numberData)
 
   const handleRollClick = () => {
-    setNumber(prev => prev.map(num => (
-      {
-        ...num,
-        number: !num.isClicked ? generateRandomNumber() : num.number
-      }
-    )))
+    setNumbers(prev =>
+      prev.map(num =>
+        num.isClicked
+          ? num
+          : { ...num, number: generateRandomNumber() }
+      )
+    )
   }
+
   const handleButtonClicked = (id: number) => {
-    setNumber(prev => {
+    console.log('dic clicked');
+
+    setNumbers(prev => {
       // get the first number value 
       const firstClicked = prev.find(n => n.isClicked)
 
@@ -36,7 +41,7 @@ const Main = () => {
 
         // if we have firstClicked already so we need check if number value for this click btn now === firstClicked value
         if (num.number === firstClicked.number) {
-          return { ...num, isClicked: true }
+          return { ...num, isClicked: !num.isClicked }
         }
 
         return num
@@ -44,24 +49,17 @@ const Main = () => {
     })
   }
 
-  const isWin = () => {
-    return numbers.every(num => (
-      num.isClicked === true
-    ))
-  }
 
-  useEffect(() => {
-    isWin() ? setUserIsWin(true) : null
-  }, [numbers])
+  const userIsWin = numbers.every(num => num.isClicked)
 
   const handlePlayAgainClick = () => {
-    setNumber(prev => prev.map(num => (
-      {
+    setNumbers(
+      numberData.map(num => ({
         ...num,
         isClicked: false,
         number: generateRandomNumber()
-      }
-    )))
+      }))
+    )
   }
 
   return (
@@ -72,20 +70,12 @@ const Main = () => {
           <Button key={num.id} num={num} handleButtonClicked={handleButtonClicked} />
         ))}
       </section>
-      {!UserIsWin ? <button
-        onClick={handleRollClick}
+      <button
+        onClick={userIsWin ? handlePlayAgainClick : handleRollClick}
         className='bg-cyan-500 text-zinc-100 p-4 text-2xl w-full rounded-xl tracking-widest font-medium'
       >
-        Roll
+        {userIsWin ? 'Play again' : 'Roll'}
       </button>
-        :
-        <button
-          onClick={handlePlayAgainClick}
-          className='bg-cyan-500 text-zinc-100 p-4 text-2xl w-full rounded-xl tracking-widest font-medium'
-        >
-          play again
-        </button>
-      }
     </main>
   )
 }
